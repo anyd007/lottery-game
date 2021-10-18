f711dfa4119ec1e587bdae5fa7d54b724b892bb6
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-18 11:11:19 +0200

    wprowadzanie zmian losowania

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const titleH1 = document.querySelector("h1");
 const newUsr = document.querySelector("h2");
+const numerShow = document.querySelector(".first-number");
+const numberShow2 = document.querySelector(".secend-number");
 const para = document.querySelector(".winner");
 const btns = document.querySelectorAll("button");
-const first = document.querySelector("button:first-child");
+const first = document.querySelector(".btn-1");
 const sec = document.querySelector(".btn-2");
 let getNum = document.querySelector(".lottery-input");
 let addNumBtn = document.querySelector(".btn-add");
+let addNumBtn2 = document.querySelector(".btn-add2");
 let addName = document.querySelector(".add-name");
 let player1Input = document.querySelector(".player-1");
 let player2Input = document.querySelector(".player-2");
@@ -13,30 +16,54 @@ const lotteryInput = /[0-9]/;
 const arr = [];
 const arr2 = [];
 const arr3 = [];
- const inputMaxLenght = 10
-para.textContent = "WPROWADŹ CYFRĘ OD 1 DO 9";
+const inputMaxLenght = 10;
 titleH1.textContent = "Losowanie".toUpperCase();
 
-btns.forEach((btn) => {
-  btn.textContent = "START";
-});
+// btns.forEach((btn) => {
+//   btn.textContent = "START";
+// });
+first.textContent = "gracz nr1".toUpperCase();
+sec.textContent = "gracz nr2".toUpperCase();
 addNumBtn.textContent = "DODAJ";
+addNumBtn2.textContent = "DODAJ";
 
-const enterEventPlayer1 = (e) => { //nadanie aktywnoiści enter na pole do wpisania imienia player1
+const enterEventPlayer1 = (e) => {
+  //nadanie aktywnoiści enter na pole do wpisania imienia player1
   if (e.key === "Enter") {
     addNameByPlayer();
   }
 };
-
-const addNameByPlayer = () => { //przypisywanie nazwy uzytkownika do pola button1
-  if (player1Input.value !== "") {
-    first.textContent = player1Input.value.toUpperCase();
+const enterEventPlayer2 = (e) => {
+  if (e.key === "Enter") {
+    addNameByPlayer2();
   }
 };
 
+const addNameByPlayer = () => {
+  //przypisywanie nazwy uzytkownika do pola button1
+  if (player1Input.value !== "") {
+    let firstName = (first.textContent = player1Input.value.toUpperCase());
+    player1Input.value = "";
+    addNumBtn.classList.add("show");
+    getNum.classList.add("show");
+    para.classList.add("show");
+    para.textContent = `${firstName} WPROWADŹ CYFRĘ OD 1 DO 9`;
+  }
+};
+const addNameByPlayer2 = () => {
+  if (player2Input.value !== "") {
+    let secendName = (sec.textContent = player2Input.value.toUpperCase());
+    player2Input.value = "";
+    addNumBtn2.classList.add("show");
+    getNum.classList.add("show");
+    para.classList.add("show");
+    para.textContent = `${secendName} WPROWADŹ CYFRĘ OD 1 DO 9`;
+  }
+};
 const user1 = () => {
+  //losowane cyfry od 1 do 9 dla gracza nr 1
   if (getNum.value.match(lotteryInput)) {
-    first.textContent = getNum.value;
+    numerShow.textContent = getNum.value;
     getNum.value = "";
   } else {
     para.textContent = `Podałeś wartość "${getNum.value}", wprowadź cyfrę w zakresie 1-9`;
@@ -45,10 +72,17 @@ const user1 = () => {
   }
 };
 const user2 = () => {
-  sec.textContent = getNum.value;
+  if (getNum.value.match(lotteryInput)) {
+    numberShow2.textContent = getNum.value;
+    getNum.value = "";
+  } else {
+    para.textContent = `Podałeś wartość "${getNum.value}", wprowadź cyfrę w zakresie 1-9`;
+    getNum.value = "";
+    setTimeout(errMeg, 3000);
+  }
 };
-
 const check = (e) => {
+  //event na klikniecie buttona z odpowiednia klasą
   if (e.target.matches(".btn-1")) {
     winBtn1();
   }
@@ -57,7 +91,8 @@ const check = (e) => {
   }
 };
 const winBtn2 = () => {
-  lotto2 = sec.textContent = Math.floor(Math.random() * 10);
+  //mechanizm losowania cyfry od 1 do 9
+  let lotto2 = (sec.textContent = Math.floor(Math.random() * 10));
   if (lotto2 === 5) {
     newUsr.textContent = `trafiłeś  ${lotto2}!! Wygrałeś`;
     setTimeout(clear, 3000);
@@ -83,8 +118,8 @@ const errMeg = () => (para.textContent = "WPROWADŹ CYFRĘ OD 1 DO 9");
 const winClear = () => (para.textContent = "");
 const clear = () => (newUsr.textContent = "");
 
-
-
-player1Input.addEventListener("keyup", enterEventPlayer1),
-  addNumBtn.addEventListener("click", user1);
+player1Input.addEventListener("keyup", enterEventPlayer1);
+player2Input.addEventListener("keyup", enterEventPlayer2);
+addNumBtn2.addEventListener('click', user2);
+addNumBtn.addEventListener("click", user1);
 btns.forEach((btn) => btn.addEventListener("click", check));
