c192d23b1661c1f9d337f29a9afa9fcd0d19a2d4
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-18 13:09:36 +0200

    poprawki

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const titleH1 = document.querySelector("h1");
 const newUsr = document.querySelector("h2");
 const numerShow = document.querySelector(".first-number");
 const numberShow2 = document.querySelector(".secend-number");
+let fullName1 = document.querySelector('.name-one');
+let fullName2 = document.querySelector('.name-sec');
 const para = document.querySelector(".winner");
 const btns = document.querySelectorAll("button");
 const first = document.querySelector(".btn-1");
@@ -22,8 +24,8 @@ titleH1.textContent = "Losowanie".toUpperCase();
 // btns.forEach((btn) => {
 //   btn.textContent = "START";
 // });
-first.textContent = "gracz nr1".toUpperCase();
-sec.textContent = "gracz nr2".toUpperCase();
+first.textContent = "losuj".toUpperCase();
+sec.textContent = "losuj".toUpperCase();
 addNumBtn.textContent = "DODAJ";
 addNumBtn2.textContent = "DODAJ";
 
@@ -42,7 +44,7 @@ const enterEventPlayer2 = (e) => {
 const addNameByPlayer = () => {
   //przypisywanie nazwy uzytkownika do pola button1
   if (player1Input.value !== "") {
-    let firstName = (first.textContent = player1Input.value.toUpperCase());
+    let firstName = (fullName1.textContent = player1Input.value.toUpperCase());
     player1Input.value = "";
     addNumBtn.classList.add("show");
     getNum.classList.add("show");
@@ -52,7 +54,7 @@ const addNameByPlayer = () => {
 };
 const addNameByPlayer2 = () => {
   if (player2Input.value !== "") {
-    let secendName = (sec.textContent = player2Input.value.toUpperCase());
+    let secendName = (fullName2.textContent = player2Input.value.toUpperCase());
     player2Input.value = "";
     addNumBtn2.classList.add("show");
     getNum.classList.add("show");
@@ -63,8 +65,10 @@ const addNameByPlayer2 = () => {
 const user1 = () => {
   //losowane cyfry od 1 do 9 dla gracza nr 1
   if (getNum.value.match(lotteryInput)) {
+    first.disabled = false;
     numerShow.textContent = getNum.value;
     getNum.value = "";
+    first.classList.add('show');
   } else {
     para.textContent = `Podałeś wartość "${getNum.value}", wprowadź cyfrę w zakresie 1-9`;
     getNum.value = "";
@@ -73,44 +77,54 @@ const user1 = () => {
 };
 const user2 = () => {
   if (getNum.value.match(lotteryInput)) {
+    sec.disabled = false;
     numberShow2.textContent = getNum.value;
     getNum.value = "";
+    sec.classList.add('show');
   } else {
     para.textContent = `Podałeś wartość "${getNum.value}", wprowadź cyfrę w zakresie 1-9`;
     getNum.value = "";
     setTimeout(errMeg, 3000);
   }
 };
-const check = (e) => {
-  //event na klikniecie buttona z odpowiednia klasą
-  if (e.target.matches(".btn-1")) {
-    winBtn1();
-  }
-  if (e.target.matches(".btn-2")) {
-    winBtn2();
-  }
-};
+
+
+    const check = (e) => {
+      //event na klikniecie buttona z odpowiednia klasą
+      if (e.target.matches(".btn-1")) {
+        winBtn1();
+      }
+      if (e.target.matches(".btn-2")) {
+        winBtn2();
+      }
+    };
+
+
 const winBtn2 = () => {
   //mechanizm losowania cyfry od 1 do 9
   let lotto2 = (sec.textContent = Math.floor(Math.random() * 10));
-  if (lotto2 === 5) {
-    newUsr.textContent = `trafiłeś  ${lotto2}!! Wygrałeś`;
-    setTimeout(clear, 3000);
+  if (sec.textContent === numberShow2.textContent) {
+    newUsr.textContent = `BRAWO ${fullName2.textContent}  trafiłeś  ${lotto2}!! Wygrałeś`;
+    setTimeout(clear, 5000);
     para.textContent = `trafiłeś wybraną cyfrę za ${arr2.length} razem`;
     arr2.length = [];
-    setTimeout(winClear, 3000);
+    setTimeout(winClear, 7000);
+    sec.textContent = 'WYGRANA'
+    sec.disabled = true;
   }
   arr2.push(lotto2);
 };
 
 const winBtn1 = () => {
   let lotto3 = (first.textContent = Math.floor(Math.random() * 10));
-  if (lotto3 === 8) {
-    newUsr.textContent = `trafiłeś ${lotto3} wygrałeś`;
-    setTimeout(clear, 3000);
+  if (first.textContent === numerShow.textContent) {
+    newUsr.textContent = `BRAWO ${fullName1.textContent} trafiłeś ${lotto3} wygrałeś`;
+    setTimeout(clear, 5000);
     para.textContent = `trafiłeś wybraną cyfrę za ${arr3.length} razem`;
     arr3.length = [];
-    setTimeout(winClear, 3000);
+    setTimeout(winClear, 7000);
+    first.textContent = 'WYGRANA';
+    first.disabled = true;
   }
   arr3.push(lotto3);
 };
